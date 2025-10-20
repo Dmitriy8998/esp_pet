f735290ac79305b8e2a3f73c7f71947fb3de43f7
Dmitriy8998 <dmitry.chistov@2lemetry.io>
2025-10-20 20:28:00 +0300

    integrare WS2812

--- board/8266_client/8266_client.ino
+++ board/8266_client/8266_client.ino
@@ -1,4 +1,4 @@
-//esp8266-07
+//esp8266-mod07
 
 #include <Arduino.h>
 
@@ -7,17 +7,24 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 
+#include <Adafruit_NeoPixel.h>
+
 #define HOST_IP "192.168.0.58:5000" //temporrary IP
 #define ssid "empty"
 #define password "empty"
 
-#define LED_12 12
+#define PIN_WS2812B   4
+#define LED_12        12
+
+#define NUM_PIXELS    3
 
 ESP8266WiFiMulti WiFiMulti;
+Adafruit_NeoPixel WS2812B(NUM_PIXELS, PIN_WS2812B, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200);
-  
+  WS2812B.begin();
+
   pinMode(LED_12, OUTPUT);
 
   WiFi.mode(WIFI_STA);
@@ -35,18 +42,26 @@ void setup() {
 }
 
 void loop() {
+  WS2812B.clear();
+
   digitalWrite(LED_12, LOW);
-  delay(1000);
+
+  for (int pixel = -1; pixel < NUM_PIXELS; pixel++) { 
+    WS2812B.setPixelColor(pixel, WS2812B.Color(0, 255, 0));
+    WS2812B.show();
+    
+    delay(300);
+  }
+  
   digitalWrite(LED_12, HIGH);
-  delay(1000);
+  delay(500);
 
   WiFiClient client;
   HTTPClient http;
 
-  //  POST request on HOST
   http.begin(client, "http://" HOST_IP "/esp/");
   http.addHeader("Content-Type", "application/json");
-  int httpCode = http.POST("{\"hello\":\"world\"}");
+  int httpCode = http.POST("{\"data\":\"Hello ESP8266\"}");
 
   Serial.print("request status: ");
   Serial.println(httpCode);
@@ -54,4 +69,7 @@ void loop() {
   http.end();
 
   delay(1000);
+
+  WS2812B.clear();
+  WS2812B.show();
 }
